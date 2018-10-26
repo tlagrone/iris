3e3e6cdca8bdffd9686df7ffb50b1606180affb6
tlagrone <travis.lagrone@trintech.com>
2018-10-26 18:42:42 -0500

    Define `main()` for CLI

--- code/preprocess/preprocess_raw_data.py
+++ code/preprocess/preprocess_raw_data.py
@@ -1,5 +1,6 @@
 
 
+import csv
 from typing import Tuple
 
 import numpy as np
@@ -10,3 +11,25 @@ def preprocess(features: np.ndarray, target: np.ndarray) -> Tuple[np.ndarray, np
     features = power_transform(features, standardize=True)
     target = label_binarize(target, np.unique(target))
     return features, target
+
+
+def main():
+    features = np.fromfile('Data/raw/data.csv', dtype=np.dtype(int), sep=',')
+    target = np.fromfile('Data/raw/target.csv', dtype=np.dtype(int), sep=',')
+
+    features = np.reshape(features, (len(target), -1))
+
+    features, target = preprocess(features, target)
+
+    with open('Data/preprocessed/features.csv', 'w', newline='') as f:
+        w = csv.writer(f)
+        for row in (features[i,:] for i in range(features.shape[0])):
+            w.writerow(row)
+    with open('Data/preprocessed/target.csv', 'w', newline='') as f:
+        w = csv.writer(f)
+        for row in (target[i,:] for i in range(features.shape[0])):
+            w.writerow(row)
+
+
+if __name__ == '__main__':
+    main()
