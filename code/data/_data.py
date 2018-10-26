190c573cc02d2a75431e9234d145f22346b5e58d
tlagrone <travis.lagrone@trintech.com>
2018-10-26 17:40:05 -0500

    Define read raw features function

--- code/data/_data.py
+++ code/data/_data.py
@@ -7,12 +7,20 @@ import numpy as np
 import pandas as pd
 
 
-def read_target(fp: Union[str, Path]) -> pd.Series:
-    data = np.fromfile(fp, dtype=np.dtype(int), sep=',')
-    target = pd.Categorical(data)
-
+def read_raw_target() -> pd.Series:
+    data = np.fromfile('Data/raw/target.csv', dtype=np.dtype(int), sep=',')
     names = np.fromfile('Data/raw/target_names.csv', dtype=np.dtype(str), sep='\n')
-    target = target.rename_categories({code: name for code, name in enumerate(names)})
 
+    target = pd.Categorical(data)
+    target = target.rename_categories({code: name for code, name in enumerate(names)})
     target = pd.Series(target, name='species')
     return target
+
+
+def read_raw_features() -> pd.DataFrame:
+    data = np.fromfile('Data/raw/data.csv', dtype=np.dtype(int), sep=',')
+    names = np.fromfile('Data/raw/feature_names.txt', dtype=np.dtype(str), sep='\n')
+
+    features = data.reshape((-1, len(names)))
+    features = pd.DataFrame(data, columns=names)
+    return features
